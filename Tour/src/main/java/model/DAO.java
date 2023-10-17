b1c257e72c0c3a5048b3f9f312e3210a175c976b
jp <developerjotape@gmail.com>
2023-10-16 21:36:26 -0300

    Adicionada: Classe DAO

---
+++ Tour/src/main/java/model/DAO.java
@@ -0,0 +1,20 @@
+package model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DAO {
+
+	public Connection conexaoBD() throws ClassNotFoundException {
+		Connection con = null;
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			String url = "jdbc:mysql://localhost:3306/itourism?user=root&password=";
+			con = DriverManager.getConnection(url);
+		} catch (Exception e) {
+			
+		}
+		return con;
+	}
+	
+}
